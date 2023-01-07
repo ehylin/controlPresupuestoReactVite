8880718c286fb4b14bea6a31cb04df96085fc52e
Ehylin <ehylinolezjua@gmail.com>
2023-01-07 17:46:38 +0100

    proyecto react vite gastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
+import Filtros from './components/Filtros'
+import ListadoGasto from './components/ListadoGastos'
 import Modal from './components/Modal'
 import { generarId } from './helpers'
 import IconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 
-
 function App() {
 
+  const [gastos, setGastos] = useState(
+    localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+  )
 
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  )
+
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
   const [modal, setModal] = useState(false)
@@ -16,9 +23,47 @@ function App() {
 
   const [gastoEditar, setGastoEditar] = useState({})
 
+  const [filtro, setFiltro] = useState('')
+  const [gastosFiltrados, setGastosFiltrados] = useState([])
+
+  useEffect(() => {
+    if (Object.keys(gastoEditar).length > 0) {
+      setModal(true)
+
+      setTimeout(() => {
+        setAnimarModal(true)
+      }, 500);
+    }
+  }, [gastoEditar])
+
+  useEffect(() => {
+    localStorage.setItem('presupuesto', presupuesto ?? 0)
+  }, [presupuesto])
+
+  useEffect(() => {
+    localStorage.setItem('gastos', JSON.stringify(gastos) ?? [])
+  }, [gastos])
+
+  useEffect(() => {
+    if (filtro) {
+      const gastosFiltrados = gastos.filter(gasto => gasto.categoria === filtro)
+      setGastosFiltrados(gastosFiltrados)
+    }
+  }, [filtro]);
+
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
+
+    if (presupuestoLS > 0) {
+      setIsValidPresupuesto(true)
+    }
+  }, []);
+
+
+
   const handleNuevoGasto = () => {
     setModal(true)
-
+    setGastoEditar({})
 
     setTimeout(() => {
       setAnimarModal(true)
@@ -43,28 +88,56 @@ function App() {
     }, 500);
   }
 
-  return (
+  const eliminarGasto = id => {
+    const gastosActualizados = gastos.filter(gasto => gasto.id !== id);
+    setGastos(gastosActualizados);
+  }
 
-    <div>
+  return (
+    <div className={modal ? 'fijar' : ''}>
       <Header
+        gastos={gastos}
+        setGastos={setGastos}
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+
       {isValidPresupuesto && (
-        <div className='nuevo-gasto'>
-          <img src={IconoNuevoGasto}
-            alt='Icono nuevo gasgo'
-            onClick={handleNuevoGasto}
-          />
-        </div>
+        <>
+          <main>
+            <Filtros
+              filtro={filtro}
+              setFiltro={setFiltro}
+            />
+            <ListadoGasto
+              gastos={gastos}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+              filtro={filtro}
+              gastosFiltrados={gastosFiltrados}
+            />
+          </main>
+          <div className="nuevo-gasto">
+            <img
+              src={IconoNuevoGasto}
+              alt="icono nuevo gasto"
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
       )}
+
       {modal && <Modal
         setModal={setModal}
         animarModal={animarModal}
         setAnimarModal={setAnimarModal}
-        guardarGasto={guardarGasto} />}
+        guardarGasto={guardarGasto}
+        gastoEditar={gastoEditar}
+        setGastoEditar={setGastoEditar}
+      />}
+
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,25 @@ import NuevoPresupuesto from './NuevoPresupuesto'
 import ControlPresupuesto from './ControlPresupuesto'
 
 const Header = ({
+    gastos,
+    setGastos,
     presupuesto,
     setPresupuesto,
     isValidPresupuesto,
-    setIsValidPresupuesto }) => {
+    setIsValidPresupuesto
+}) => {
     return (
         <header>
-            <h1>Planificador de gastos</h1>
+            <h1>Planificador de Gastos</h1>
+
             {isValidPresupuesto ? (
                 <ControlPresupuesto
-                    presupuesto={presupuesto} />
+                    gastos={gastos}
+                    setGastos={setGastos}
+                    presupuesto={presupuesto}
+                    setPresupuesto={setPresupuesto}
+                    setIsValidPresupuesto={setIsValidPresupuesto}
+                />
             ) : (
                 <NuevoPresupuesto
                     presupuesto={presupuesto}
@@ -20,6 +29,7 @@ const Header = ({
                     setIsValidPresupuesto={setIsValidPresupuesto}
                 />
             )}
+
         </header>
     )
 }
